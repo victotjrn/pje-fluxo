a06d0db83fea80caa62f56c4e25a75606adfc217
Paulo Porto <pauloporto@tjrn.jus.br>
2016-02-26 13:19:00 -0300

    Criado script para realização de backup dos fluxos do banco para arquivos XML's.

---
+++ bkp-fluxo.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# coding: utf-8
+
+import psycopg2
+import os
+import shutil
+import datetime
+import env
+from datetime import datetime
+
+con = None
+
+try:
+    con = psycopg2.connect(host=env.DB_HOST, user=env.DB_USER, password=env.DB_PASS, database=env.DB_NAME, port=env.DB_PORT)
+
+    cur = con.cursor()
+    cur.execute("SELECT ds_fluxo, ds_xml FROM tb_fluxo WHERE ds_fluxo ilike '" + env.SQL_LIKE_CRITERIA + "%'")
+except psycopg2.DatabaseError, e:
+    print 'Erro ao tentar conexão com o banco: %s' % e
+    sys.exit(1)
+
+if cur.rowcount == 0:
+    print "Nenhum fluxo foi encontrado com os critérios informados. Backup não realizado!"
+    raise SystemExit(0)
+
+rows = cur.fetchall()
+
+dst_dir = env.PATH_DESTINATION
+
+if not os.path.exists(dst_dir):
+    os.makedirs(dst_dir)
+
+for row in rows:
+    file = open(dst_dir + '/' + row[0].decode('latin1') + '.xml', "w")
+    file.write(row[1])
+    file.close()
+
+print "Backup realizado com sucesso!"
---
+++ env-example.py
@@ -0,0 +1,14 @@
+# Make a copy this file to env.py and set your configurations
+
+# Database configurations
+DB_HOST="127.0.0.1"
+DB_USER="postgres"
+DB_PASS="postgres"
+DB_NAME="pje2grau"
+DB_PORT="5432"
+
+# Used for like criteria
+SQL_LIKE_CRITERIA="(SG)"
+
+# Path destination to save files
+PATH_DESTINATION="segundograu/sg"
