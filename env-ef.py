049f8513fe218d608e46c1e0391d496f6c3312f0
luan <luan@luan.intrajus.tjrn>
2018-07-11 09:35:20 -0300

    Colocando fluxos que estão em produção; Colocando alterações necessarias devido a mudança de versão;

--- env-ef.py
+++ env-ef.py
@@ -1,14 +1,14 @@
 # Make a copy this file to env.py and set your configurations
 
 # Database configurations
-DB_HOST="t-zimbabwe.intrajus.tjrn"
-DB_USER="postgres"
-DB_PASS="z63Qqv7733Ed7MR"
+DB_HOST="z-zagreb.tjrn.jus.br"
+DB_USER="luan"
+DB_PASS="luan@hmlrw"
 DB_NAME="pje1grau_"
 DB_PORT="5432"
 
 # Used for like criteria
-SQL_LIKE_CRITERIA="EF_"
+SQL_LIKE_CRITERIA=""
 
 # Path destination to save files
 PATH_DESTINATION="primeirograu/ef"
